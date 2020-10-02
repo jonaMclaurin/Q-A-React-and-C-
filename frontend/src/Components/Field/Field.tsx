53c88ee443596211a6fda1462966aae3495a2fa7
jonaMclaurin <jonatan_lqfb@hotmail.com>
2020-10-02 01:58:49 -0500

    added generic field and form components

---
+++ frontend/src/Components/Field/Field.tsx
@@ -0,0 +1,86 @@
+// eslint-disable-next-line
+import React, { FC, useContext, ChangeEvent } from 'react';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { fontFamily, fontSize, gray5, gray2, gray6 } from '../../Styles/Styles';
+import { FormContext } from '../Form/Form';
+
+interface Props {
+  name: string;
+  label?: string;
+  type?: 'Text' | 'TextArea' | 'Password';
+}
+
+export const Field: FC<Props> = ({ name, label, type = 'Text' }) => {
+  const { setValue } = useContext(FormContext);
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    if (setValue) {
+      setValue(name, e.currentTarget.value);
+    }
+  };
+  return (
+    <FormContext.Consumer>
+      {({ values }) => (
+        <div
+          css={css`
+            display: flex;
+            flex-direction: column;
+            margin-bottom: 15px;
+          `}
+        >
+          {label && (
+            <label
+              css={css`
+                font-weight: bold;
+              `}
+              htmlFor={name}
+            >
+              {label}
+            </label>
+          )}
+          {(type === 'Text' || type === 'Password') && (
+            <input
+              value={values[name] === undefined ? '' : values[name]}
+              onChange={handleChange}
+              type={type.toLowerCase()}
+              id={name}
+              css={baseCSS}
+            />
+          )}
+          {type === 'TextArea' && (
+            <textarea
+              id={name}
+              value={values[name] === undefined ? '' : values[name]}
+              onChange={handleChange}
+              css={css`
+                ${baseCSS};
+                height: 100px;
+              `}
+            />
+          )}
+        </div>
+      )}
+    </FormContext.Consumer>
+  );
+};
+
+const baseCSS = css`
+  box-sizing: border-box;
+  font-family: ${fontFamily};
+  font-size: ${fontSize};
+  margin-bottom: 5px;
+  padding: 8px 10px;
+  border: 1px solid ${gray5};
+  border-radius: 3px;
+  color: ${gray2};
+  background-color: white;
+  width: 100%;
+  :focus {
+    outline-color: ${gray5};
+  }
+  :disabled {
+    background-color: ${gray6};
+  }
+`;
---
+++ frontend/src/Components/Form/Form.tsx
@@ -0,0 +1,61 @@
+// eslint-disable-next-line
+import React, { FC, useState, createContext } from 'react';
+import { PrimaryButton, gray5, gray6 } from '../../Styles/Styles';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+
+export interface Values {
+  [key: string]: any;
+}
+
+interface FormContextProps {
+  values: Values;
+  setValue?: (fieldName: string, value: any) => void;
+}
+
+interface Props {
+  submitCaption?: string;
+}
+
+export const FormContext = createContext<FormContextProps>({
+  values: {},
+});
+
+export const Form: FC<Props> = ({ submitCaption, children }) => {
+  const [values, setValues] = useState<Values>({});
+  return (
+    <FormContext.Provider
+      value={{
+        values,
+        setValue: (fieldName: string, value: any) => {
+          setValues({ ...values, [fieldName]: value });
+        },
+      }}
+    >
+      <form noValidate={true}>
+        <fieldset
+          css={css`
+            margin: 10px auto 0 auto;
+            padding: 30px;
+            width: 350px;
+            background-color: ${gray6};
+            border-radius: 4px;
+            border: 1px solid ${gray5};
+            box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+          `}
+        >
+          {children}
+          <div
+            css={css`
+              margin: 30px 0px 0px 0px;
+              padding: 20px 0px 0px 0px;
+              border-top: 1px solid ${gray5};
+            `}
+          >
+            <PrimaryButton type="submit">{submitCaption}</PrimaryButton>
+          </div>
+        </fieldset>
+      </form>
+    </FormContext.Provider>
+  );
+};
